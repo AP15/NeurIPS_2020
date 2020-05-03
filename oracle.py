beed7e8a55feef06be3945cbe097c100762f5621
AP15 <and.paudice@gmail.com>
2020-05-03 17:33:37 +0200

    Add files via upload

---
+++ oracle.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 18:14:26 2020
+
+@author: apaudice
+"""
+
+import numpy as np
+
+class SCQ0racle(object):
+    """Same-Cluster-Query (SCQ) oracle.
+    Given any pair (x,y) \in X, return True if C(x)=C(y) and False o.w.
+    Counts also the number of asked SCQs so far.
+    
+    Attributes
+    ----------
+    data : pandas dataframe, shape = [#Points, #Features+1]
+        Input data for clustering.
+    dim : int
+        #Features = #Columns of data.  
+    count : int
+        #SCQs asked so far.
+    foundClusters: list, shape <= #Clusters in data
+        Labels of the clusters for which at least a member has been found.
+        
+    Methods
+    -------
+    __init__(data : pandas dataframe)
+        Construtor
+    scq(idx : int, idy : int)
+        Return SCQ answer for (idx, idy).
+    label(idx : int)
+        Return the cluster membership label of idx.
+    getCount()
+        Return #SCQs asked so far.
+    getFoundClusters()
+    """
+    
+    def __init__(self, data):
+        self.data = data.copy()
+        self.dim = len(data.columns)
+        self.count = 0
+        self.foundClusters = []
+        
+    
+    def scq(self, idx, idy):
+        self.count += 1
+        return self.data[self.dim-1][idx] == self.data[self.dim-1][idy]
+    
+    
+    def label(self, idx):
+        label_x = self.data[self.dim-1][idx]
+        if label_x in self.foundClusters:
+            self.count += self.foundClusters.index(label_x)
+        else:
+            self.count += len(self.foundClusters)
+            self.foundClusters.append(label_x)
+            self.foundClusters.sort()
+        return label_x
+    
+    
+    def getCount(self):
+        return self.count
+    
+    
---
+++ testOracle.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 19:16:08 2020
+
+@author: apaudice
+"""
+
+import oracle
+import pandas as pd
+
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                 'machine-learning-databases/iris/iris.data', header=None)
+df.tail()
+
+batgirl = oracle.SCQ0racle(df)
+
+#Test scq
+print(df[4][0], df[4][30])
+print(batgirl.scq(1, 30))
+print(batgirl.scq(5, 70))
+print(batgirl.scq(15, 90))
+
+#Test getCount
+print('Count: ', batgirl.getCount())
+batgirl.getFoundClusters()
+
+#Test label
+print(batgirl.label(2))
+print('Count: ', batgirl.getCount())
+
+print(batgirl.label(80))
+print('Count: ', batgirl.getCount())
+
+print(batgirl.label(120))
+print('Count: ', batgirl.getCount())
