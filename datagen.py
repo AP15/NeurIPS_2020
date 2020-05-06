b7c54f61f01de8b158afd3bab7fdcf028430fb32
Marco Bressan <bressan@di.uniroma1.it>
2020-05-06 21:21:27 +0200

    Added main to datagen.py.

--- datagen.py
+++ datagen.py
@@ -254,3 +254,20 @@ def randomDataset(n: int, k: int, d: int, gamma: float = 0.5, r: int = None, cn:
     return X, y.astype(int), Ws, cs
 
 
+if __name__ == '__main__':
+    # Sample usage
+    import matplotlib.pyplot as plt
+    print("generating clusters...")
+    X,y,Ws,cs=randomDataset(n=1000, k=5, d=2, r=2, gamma=.5, cn=10, tightMargin=True)
+    fig=plt.figure(figsize=(5, 5))
+    fig.add_subplot(111).scatter(X[:, 0], X[:, 1], c=y, s=1)
+    if False: # plot each cluster in its latent space
+        for i in np.unique(y):
+            Xt=toLatent(X, Ws[i], cs[i])
+            fig=plt.figure()
+            ax=fig.add_subplot(111)
+            C=Xt[y==i]
+            ax.scatter(Xt[:, 0], Xt[:, 1], c=y, s=3)
+            ax.set_xlim(4*C[:, 0].min(), 4*C[:, 0].max())
+            ax.set_ylim(4*C[:, 1].min(), 4*C[:, 1].max())
+    plt.show()
